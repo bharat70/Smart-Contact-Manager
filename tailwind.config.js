9d8fb7f75d26bbd19dc07bbe7ce973d261349625
Bharat Sharma <arjunsharma1492002@gmail.com>
2024-10-13 12:49:18 +0530

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  'darkMode': 'selector',
+  content: ["./src/main/resources/**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
